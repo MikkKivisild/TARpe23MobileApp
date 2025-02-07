3c007a0b60c6426ef14ee22dd29f2e134137eb48
Opilane TTHK <opilane@tthk.ee>
2025-02-07 14:12:15 +0200

    planets

--- mauiPlanets/mauiPlanets/AppShell.xaml.cs
+++ mauiPlanets/mauiPlanets/AppShell.xaml.cs
@@ -8,24 +8,10 @@ namespace mauiPlanets
 {
     public partial class AppShell : Shell
     {
-        const int WindowWidth = 540;
-        const int WindowHeight = 1000;
+        
         public AppShell()
         {
             InitializeComponent();
-
-            Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
-            {
-                #if WINDOWS
-                var mauiWindow = handler.VirtualView;
-                var nativeWindow = handler.PlatformView;
-                nativeWindow.Activate();
-                IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
-                WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
-                AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
-                appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
-                #endif
-            });
 
         }
     }
