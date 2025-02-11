16a9a5710dc8ca24599983fb1d2a7a1a2cdafc14
Opilane TTHK <opilane@tthk.ee>
2025-02-11 14:11:29 +0200

    xbbxcb

--- mauiPlanets/mauiPlanets/Views/PlanetsPage.xaml.cs
+++ mauiPlanets/mauiPlanets/Views/PlanetsPage.xaml.cs
@@ -1,9 +1,38 @@
+using Services;
+using Models;
+
 namespace Views;
 
 public partial class PlanetsPage : ContentPage
 {
-	public PlanetsPage()
-	{
-		InitializeComponent();
-	}
+    private const uint AnimationDuration = 800u;
+    public PlanetsPage()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        lstPopularPlanets.ItemsSource = PlanetsService.GetFeaturedPlanets();
+        lstAllPlanets.ItemsSource = PlanetsService.GetAllPlanets();
+    }
+
+    async void Planets_SelectionChanged(System.Object sender, Microsoft.Maui.Controls.SelectionChangedEventArgs e)
+    {
+        await Navigation.PushAsync(new PlanetsDetailsPage(e.CurrentSelection.First() as Planet));
+    }
+    async void GridArea_Tapped(System.Object sender, System.EventArgs e)
+    {
+        await CloseMenu();
+    }
+
+    private async Task CloseMenu()
+    {
+        _ = MainContentGrid.FadeTo(1, AnimationDuration);
+        _ = MainContentGrid.ScaleTo(1, AnimationDuration);
+        await MainContentGrid.TranslateTo(0, 0, AnimationDuration, Easing.CubicIn);
+    }
+
 }
